9366f669e5f6565b883dcbb36bdd7c34c4098efc
René Föhring <rf@bamaru.de>
2015-08-29 14:38:55 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Timex.Mixfile do
 
   def project do
     [ app: :timex,
-      version: "0.19.2",
+      version: "0.20.0-dev",
       elixir: "~> 1.0",
       description: "A date/time library for Elixir",
       package: package,
